0d5023117f4583a47282da42533af37c9522a69d
EC2 Default User <ec2-user@ip-172-31-18-116.ap-northeast-1.compute.internal>
2022-08-03 14:38:41 +0000

    [Running]task

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,11 +1,21 @@
 class BooksController < ApplicationController
   
   def new
-    @books = Book.new  
+    @book = Book.new
+  end  
+  
+  def create
+    @book = Book.new(book_params)
+     if @book.save
+     redirect_to book_path(@book.id),notice: 'Book was successfully created.'
+     else
+     @books = Book.all
+     render :index 
+     end
   end
   
   def index
-    @books = Book.all  
+    @books = Book.all
   end
 
   def show
@@ -16,30 +26,24 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
   end
 
-  def create
-        # １.&2. データを受け取り新規登録するためのインスタンス作成
-    book = Book.new(book_params)
-    # 3. データをデータベースに保存するためのsaveメソッド実行
-    book.save
-    # 4. トップ画面へリダイレクト
-    redirect_to book_path(book.id)  
-  end
-
   def destroy
-     book = Book.find(params[:id])  # データ（レコード）を1件取得
-    book.destroy  # データ（レコード）を削除
-    redirect_to '/books'  # 投稿一覧画面へリダイレクト  
+    book = Book.find(params[:id]) 
+    book.destroy
+    redirect_to '/books'
   end
 
   def update
-     book = Book.find(params[:id])
-     book.update(book_params)
-     redirect_to book_path(book.id)  
+    @book = Book.find(params[:id])
+     if @book.update(book_params)
+     redirect_to book_path(@book.id),notice: 'Book was successfully updated.'
+     else
+     render:index
+     end
   end
   
-   private
+  private
   def book_params
-    params.require(:book).permit(:title, :body)
+    params.permit(:title, :body)
   end
   
 end
